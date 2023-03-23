c5df39d1414b9fa5199de4fa77b50d23a871df34
EisenhornE <mahesvara31@gmail.com>
2023-03-23 10:56:34 +0800

    feat: added videos section
    TODO: fix svelte error in Videos.svelte

---
+++ src/components/Videos.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  let selectedUrl: string = "KGzI69BGOL8";
+</script>
+
+<section class="">
+  <!-- Iframe -->
+  <div class="video-container w-1/2 mx-auto">
+    <iframe
+      title="test"
+      width="560"
+      height="315"
+      src={`https://www.youtube-nocookie.com/embed/${selectedUrl}`}
+      frameborder="0"
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      allowfullscreen
+    />
+  </div>
+
+  <!-- Select Video -->
+  <div class="w-1/4 mx-auto pt-3 flex gap-4">
+    <button
+      class="px-4 py-2 bg-blue-500"
+      on:click={() => (selectedUrl = "G7AFWdH3Qzg")}
+      >1st
+    </button>
+    <button
+      class="px-4 py-2 bg-blue-500"
+      on:click={() => (selectedUrl = "gcEc0qS2pxs")}
+      >2nd
+    </button>
+    <button
+      class="px-4 py-2 bg-blue-500"
+      on:click={() => (selectedUrl = "S-HZDq1DWUQ")}
+      >3rd
+    </button>
+    <button
+      class="px-4 py-2 bg-blue-500"
+      on:click={() => (selectedUrl = "KGzI69BGOL8")}
+      >4th
+    </button>
+  </div>
+</section>
+
+<!-- TODO: Convert every links to images. Svelte error Ln 2, 33, 38: Unknown word error -->
