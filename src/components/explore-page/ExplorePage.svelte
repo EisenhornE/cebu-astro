74f5b0f0cc32634ed12a1a5dbed0118650b826e6
EisenhornE <mahesvara31@gmail.com>
2023-04-25 12:01:54 +0800

    refactor; removed explorepage json. refactor: renamed ExplorePage.astro to .svelte

---
+++ src/components/explore-page/ExplorePage.svelte
@@ -0,0 +1,20 @@
+--- ---
+
+<section class="pt-12">
+  <div class="pt-12">
+    <div class="h-1/2">
+      <img
+        src="trending-pics/18.png"
+        alt="Cebu Activities"
+        class="w-full h-full object-cover"
+      />
+    </div>
+    <div>
+      <h1
+        class="bg-[#2B5F8F] inline-block absolute top-16 left-16 text-5xl p-4 text-white rounded-tr-"
+      >
+        Explore Cebu!
+      </h1>
+    </div>
+  </div>
+</section>
