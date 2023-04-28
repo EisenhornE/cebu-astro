baf45688b6564b1ff215ee8ed02a039689c755c2
EisenhornE <mahesvara31@gmail.com>
2023-04-28 14:03:43 +0800

    feat: added a video page

---
+++ src/components/video-page/VideosPage.svelte
@@ -0,0 +1,28 @@
+<script>
+  let videoURL = "";
+</script>
+
+<section class="">
+  <div>
+    <div class="flex">
+      <img src="" alt="" />
+    </div>
+    <div class="text-center py-16 border-2 border-black">
+      <h1
+        class="inline-block border-b-4 border-border-green ml-2 mb-8 pt-16 text-2xl"
+      >
+        Videos from Cebu
+      </h1>
+      <p class="ml-2">
+        Welcome to our website's video gallery, where you can explore
+        captivating content featuring bloggers' adventures and captivating
+        tourism ads showcasing the beauty of Cebu. Immerse yourself in the
+        vibrant culture, stunning landscapes, and awe-inspiring attractions of
+        this enchanting Philippine province. Our curated collection brings
+        together an array of videos that capture the essence of Cebu's diverse
+        destinations, from pristine white-sand beaches to lush mountains and
+        bustling cityscapes.
+      </p>
+    </div>
+  </div>
+</section>
