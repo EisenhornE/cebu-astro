76bdee2028dfc14f9d1b88850216d46bbd5215b3
EisenhornE <mahesvara31@gmail.com>
2023-05-10 11:43:26 +0800

    feat: added >1024 view

--- src/components/video-page/VideosPage.svelte
+++ src/components/video-page/VideosPage.svelte
@@ -28,11 +28,11 @@
 <section>
   {#each videosJson.videos as videos}
     <div class="flex py-12">
-      <div class="w-1/2">
+      <div class="w-full">
         <!-- Iframe -->
-        <div class="video-container mx-auto p-0 w-fit-content pl-2">
+        <div class="video-container mx-auto p-0 w-fit-content ml-12">
           <iframe
-            class="lg:w-[50rem] h-[25rem] sm:w-full"
+            class="lg:w-full h-[25rem] sm:w-full"
             title="test"
             src={`https://www.youtube-nocookie.com/embed/${videos.url}`}
             frameborder="0"
@@ -41,9 +41,13 @@
           />
         </div>
       </div>
-      <div class="w-1/2">
-        <h1 class="pl-6 text-xl">{videos.name}</h1>
-        <p class="pl-6 pt-4">
+      <div class="w-3/4 lg:mx-8">
+        <h1
+          class="text-xl lg:inline-block lg:border-b-4 lg:border-border-green lg:text-xl"
+        >
+          {videos.name}
+        </h1>
+        <p class="pt-4">
           {videos.description}
         </p>
       </div>
